d07e431feaacc84cd6bc685296917dafa7cdf894
Zach Forrest <zacharundel@gmail.com>
2019-08-29 09:29:57 +0100

    commented out some unneeded modules

--- app.py
+++ app.py
@@ -3,6 +3,6 @@ from flask import request
 from flask import jsonify
 from flask import render_template
 from flask_jsglue import JSGlue
-import neural_net
-import check_api
+#import neural_net
+#import check_api
 
